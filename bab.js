012c103f39fc272d15508891e2730d99de610025
Trevor Shim <trevorshim@outlook.kr>
2017-09-12 22:49:06 -0400

    paylaod

--- bab.js
+++ bab.js
@@ -38,7 +38,11 @@ var whichSikdang = function(event, db){
           "payload": JSON.parse(body).stores[i].name
         });
       }
-      var messageData = {"text": "어디서 먹을건데?", "quick_replies": sikdang};
+      var messageData = {"text": "어디서 먹을건데?", "quick_replies": [
+        "content_type": "text",
+        "title": "학생회관",
+        "payload": "학생회관"]
+      };
 
       api.sendMessage(event, messageData);
       db.collection('users').update({ "fbuid": event.sender.id }, { $set: { "messagePriority": "sendBabMenu"} }, function(err, doc){
